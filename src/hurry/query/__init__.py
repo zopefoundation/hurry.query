b7c3ca7b66063630e3b38430412b8994f0c704d8
Martijn Faassen <faassen@startifact.com>
2008-09-16 22:01:45 +0000

    Update license headers.

--- src/hurry/query/__init__.py
+++ src/hurry/query/__init__.py
@@ -1,3 +1,15 @@
-# Copyright (c) 2007 Infrae. All rights reserved.
-# See also LICENSE.txt
+##############################################################################
+#
+# Copyright (c) 2005-2008 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
 from query import And, Or, Eq, NotEq, Between, In, Ge, Le, Text
