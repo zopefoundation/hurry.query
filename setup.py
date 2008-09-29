6536d8126ca542f9d766774d3fe84c37b1e11eee
Paul Carduner <paul@carduner.net>
2008-09-29 17:30:41 +0000

    improve setup.py and prepare for a new release.

--- setup.py
+++ setup.py
@@ -1,26 +1,60 @@
+##############################################################################
+#
+# Copyright (c) 2007 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Setup
+
+$Id: setup.py 91011 2008-09-09 22:01:02Z malthe $
+"""
+import os
 from setuptools import setup, find_packages
+
+def read(*rnames):
+    text = open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+    text = unicode(text, 'utf-8').encode('ascii', 'xmlcharrefreplace')
+    return text
 
 setup(
     name="hurry.query",
-    version="0.9.4dev",
+    version="0.9.3",
+    author='Infrae',
+    author_email='faassen@startifact.com',
+    description="""\
+hurry.query is a higher level query system built on top of the Zope 3
+catalogs. It makes it easy to perform catalog queries in Zope 3 code.
+""",
+    long_description=read('src','hurry','query','query.txt'),
+    license='ZPL 2.1',
+    keywords="zope zope3 query",
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Zope Public License',
+        'Programming Language :: Python',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: Zope3'],
+    url = 'http://pypi.python.org/pypi/hurry.query',
     packages=find_packages('src'),
     package_dir= {'':'src'},
-    
+
     namespace_packages=['hurry'],
     package_data = {
     '': ['*.txt', '*.zcml'],
     },
 
     zip_safe=False,
-    author='Infrae',
-    author_email='faassen@startifact.com',
-    description="""\
-hurry.query is a higher level query system built on top of the Zope 3
-catalog. It makes it easy to perform catalog queries in Zope 3 code.
-""",
-    license='ZPL 2.1',
-    keywords="zope zope3",
-    classifiers = ['Framework :: Zope3'],
     install_requires=[
     'zc.catalog >= 0.1.1',
     'setuptools'],
