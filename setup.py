be660bc57474ec0f4c2dcff954370ffc920473f6
Martijn Faassen <faassen@startifact.com>
2012-06-22 13:19:25 +0000

    Back to development: 1.1.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ tests_require = [
 
 setup(
     name="hurry.query",
-    version='1.1.1',
+    version='1.1.2.dev0',
     author='Infrae',
     author_email='faassen@startifact.com',
     description="Higher level query system for the zope.catalog",
