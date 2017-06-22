0c932395ac389417f0078b8e3ffd04b218c110f3
Jan-Wijbrand Kolman <jkolman@minddistrict.com>
2017-06-22 09:07:22 +0200

    Preparing release 2.4

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ tests_require = [
 
 setup(
     name="hurry.query",
-    version='2.4.dev0',
+    version='2.4',
     author='Infrae',
     author_email='faassen@startifact.com',
     description="Higher level query system for the zope.catalog",
