fbab65788a4a32435f22c90f454eb179cd9884e3
Sylvain Viollon <sviollon@minddistrict.com>
2020-11-16 16:53:34 +0100

    Preparing release 3.2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ tests_require = [
 
 setup(
     name="hurry.query",
-    version='3.2.dev0',
+    version='3.2',
     author='Infrae',
     author_email='faassen@startifact.com',
     description="Higher level query system for the zope.catalog",
